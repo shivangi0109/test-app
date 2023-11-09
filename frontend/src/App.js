a6e2a6828be54131f6475eefec73b01133931687
Shivangi Srivastava <shi06dec@gmail.com>
2023-11-09 01:03:19 -0700

    Set up front end React app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Auth with JWT</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
