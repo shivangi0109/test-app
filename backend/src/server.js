5de4c5ff27548e9cc46f673adae93167b12e2c77
Shivangi Srivastava <shi06dec@gmail.com>
2023-11-09 00:13:08 -0700

    Create POST login route and check this endpoint on postman to verify

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,9 +17,21 @@ const data = [
   { id: uniqid(), breed: 'Dachshund', image_url: 'https://images.dog.ceo/breeds/chihuahua/black_chihuahua.jpg' },
 ];
 
+const users = [
+  { id: uniqid(), email: 'test@email.com', password: 'abc123' }
+];
+
 app.get("/", (req, res) => {
   res.send('Hello, World!');
 })
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find(item => item.email === email);
+  if(!user) return res.status(404).json({ message: 'User does not exists'});
+  if(user.password !== password) return res.status(400).json({ message: 'Invalid credentials'});
+  return res.status(200).json({ message: 'Login successfully!'});
+});
 
 app.get("/dogs", (req, res) => {
   res.json(data);
