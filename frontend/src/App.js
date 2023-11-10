99c3dc66910a3a2d2b729ad9a373d5d45a35d837
Shivangi Srivastava <shi06dec@gmail.com>
2023-11-10 12:00:21 -0700

    Installed bootstrap and node-sass, created TopNavBar, customized bootstrap styles and corrected components folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+import NavigationBar from './components/NavigationBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar />
       <header className="App-header">
         <h1>Auth with JWT</h1>
       </header>
